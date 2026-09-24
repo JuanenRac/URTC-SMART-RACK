bddaa0fa1c1de6bfbfb66162014edeea5e8d70ad
Electro Hobby 3D <electrohobby3d@gmail.com>
2026-09-24 18:48:53 +0200

    chore: separate-field version patterns can carry a build entry

--- src/firmware_common.h
+++ src/firmware_common.h
@@ -25,6 +25,7 @@
 #define FIRMWARE_VERSION_MAJOR 0
 #define FIRMWARE_VERSION_MINOR 1
 #define FIRMWARE_VERSION_PATCH 0
+#define FIRMWARE_VERSION_BUILD 0
 
 // Encodes MAJOR.MINOR.PATCH as a single monotonically-increasing integer
 // (major*10000 + minor*100 + patch) - cheap to compare or report over CAN
